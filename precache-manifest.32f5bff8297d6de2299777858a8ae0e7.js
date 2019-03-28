085ec5c284d25ad5c596e9ebbd546e6022b4dd4f
Ricardo Coronado <rikardo.corp@gmail.com>
2019-03-28 15:27:46 -0500

    my page

---
+++ precache-manifest.32f5bff8297d6de2299777858a8ae0e7.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "5faeaeeeb1e4c0c7ce52c44489129b98",
+    "url": "/static/media/icomoon.5faeaeee.eot"
+  },
+  {
+    "revision": "55db963845478dd41e26",
+    "url": "/static/css/main.b1c1392f.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b69a38cd3062e5c8b765",
+    "url": "/static/js/2.f8758ab1.chunk.js"
+  },
+  {
+    "revision": "c098af367d6565e0e6a177bc4444864a",
+    "url": "/static/media/profile.c098af36.jpeg"
+  },
+  {
+    "revision": "6e335928159c0af7b1a347b9ed616aaf",
+    "url": "/static/media/react.6e335928.png"
+  },
+  {
+    "revision": "9a2fb48ebe83232d5ba4bd063c0e626c",
+    "url": "/static/media/icomoon.9a2fb48e.ttf"
+  },
+  {
+    "revision": "3371ee663942f805c02e1d5a9307f2c4",
+    "url": "/static/media/icomoon.3371ee66.woff"
+  },
+  {
+    "revision": "96cdba54a969ba2476dea4326203d7fa",
+    "url": "/static/media/icomoon.96cdba54.svg"
+  },
+  {
+    "revision": "55db963845478dd41e26",
+    "url": "/static/js/main.01b5bb95.chunk.js"
+  },
+  {
+    "revision": "e829cfaa3fe9854a976b0814dbd2732b",
+    "url": "/static/media/TerminusTTF-4.46.0.e829cfaa.ttf"
+  },
+  {
+    "revision": "69d9ace3bc866372abd0c74cb2b006c8",
+    "url": "/static/media/last_paradise-webfont.69d9ace3.ttf"
+  },
+  {
+    "revision": "ea2a82c8be4e6f4a99613f5a99cbc098",
+    "url": "/static/media/portada1.ea2a82c8.jpg"
+  },
+  {
+    "revision": "b35ea4889b4273ab02cf38e04e08e925",
+    "url": "/static/media/portada.b35ea488.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "b69a38cd3062e5c8b765",
+    "url": "/static/css/2.45d9d20f.chunk.css"
+  },
+  {
+    "revision": "dbd767a58d8b7997d172ff2859b34264",
+    "url": "/index.html"
+  }
+];
